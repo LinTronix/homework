566941980aed0383d93ce8da3d035c21fc8d0da2
linuxGitJava <tronis.janis@gmail.com>
2018-12-26 13:33:04 +0200

    cleaning unnecessary

--- src/palindrome/Main.java
+++ src/palindrome/Main.java
@@ -5,7 +5,7 @@ public class Main
     public static void main(String[] args)
     {
         String string ="Bob";
-        int a = 111;
+        int a = 112;
         System.out.println(string + " is palindrome = " + Palindr.isPalindrom(string));
         System.out.println(a + " is palindrome = " +Palindr.isPalindrom(a));
     }
